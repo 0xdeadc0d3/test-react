0f414ffed0e8afc8846dd0758284ac23ef3da15b
0xdeadc0d3 <pedrinho.forever.2108@gmail.com>
2022-11-08 14:27:26 +0200

    add tests 2

---
+++ tests/SimpleTest.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+
+test("test if true is true", () => {
+  expect(true).toBe(true);
+});
+
+test("test if false is false", () => {
+  expect(false).toBe(false);
+});
